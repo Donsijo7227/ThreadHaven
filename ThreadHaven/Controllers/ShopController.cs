d7f43bc1d97994c0126884b4a2b5b290d5d8311b
donsijo <donsi@DON>
2024-09-10 09:57:43 -0500

    shop category added

---
+++ ThreadHaven/Controllers/ShopController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThreadHaven.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Shop/ByCategory/123
+        public IActionResult ByCategory(int id)
+        {
+            if (id == 1)
+            {
+                ViewData["Category"] = "Sweaters";
+            }
+            else if (id == 2)
+            {
+                ViewData["Category"] = "Hoodies";
+            }
+            else if (id == 3)
+            {
+                ViewData["Category"] = "pants";
+            }
+            else if (id == 4)
+            {
+                ViewData["Category"] = "T-shirts";
+            }
+            else 
+            {
+                //we have no id so takes user back to shop index where they can choose a category 
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
